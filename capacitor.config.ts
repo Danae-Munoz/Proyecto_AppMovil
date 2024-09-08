057abc9e45c31ef7eacd3ffe73a6dd1236165509
Danae Muñoz Opitz <dana.munozo@duocuc.cl>
2024-09-07 22:56:55 -0400

    Elimine el proyecto

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'RegistroApp',
-  webDir: 'www'
-};
-
-export default config;
