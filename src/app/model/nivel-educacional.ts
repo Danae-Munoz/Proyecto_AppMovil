5c8ab1f6f6e878e6c4c53b116b130206eacde495
Danae Muñoz Opitz <dana.munozo@duocuc.cl>
2024-09-11 17:20:38 -0300

    Se crea los modelos de nivel.persona-usuario

--- src/app/model/nivel-educacional.ts
+++ src/app/model/nivel-educacional.ts
@@ -1,2 +1,39 @@
 export class NivelEducacional {
+    public id: number;
+    public nombre: string;
+
+    public constructor(){
+        this.id= 1;
+        this.nombre= 'Básica incompleta';
+    }
+
+    public setNivelEducacional(id:number, nombre:string): void{
+        this.id= id;
+        this.nombre= nombre;
+    }
+
+    public getNivelEducacional(id: number, nombre: string): NivelEducacional{
+        const nivel= new NivelEducacional();
+        nivel.setNivelEducacional(id,nombre)
+        return nivel;
+    }
+
+    public static getNivelesEducacionales(): NivelEducacional[]{
+        const niveles: NivelEducacional[]=[
+            new NivelEducacional().getNivelEducacional(1, 'Básica incompleta'),
+            new NivelEducacional().getNivelEducacional(2, 'Básica completa'),
+            new NivelEducacional().getNivelEducacional(3, 'Media incompleta'),
+            new NivelEducacional().getNivelEducacional(4, 'Media completa'),
+            new NivelEducacional().getNivelEducacional(5, 'Superior incompleta'),
+            new NivelEducacional().getNivelEducacional(6, 'Superior completa'),
+        ];
+        return niveles;
+    }
+    public getTextoNivelEducacional(): string{
+        return this.id.toString()+'-'+ this.nombre;
+    }
+
+    public static findNivelEducacionalById(id:number): NivelEducacional | undefined{
+        return NivelEducacional.getNivelesEducacionales().find(n => n.id === id);
+    }
 }
