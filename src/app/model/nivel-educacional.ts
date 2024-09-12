99dcdfb1bdcbc000f440956b6f3f8cb2fa272d35
Danae Muñoz Opitz <dana.munozo@duocuc.cl>
2024-09-12 01:12:05 -0300

    Mensaje de commit sobre los cambios actuales

--- src/app/model/nivel-educacional.ts
+++ src/app/model/nivel-educacional.ts
@@ -1,39 +1,42 @@
 export class NivelEducacional {
+
     public id: number;
     public nombre: string;
-
-    public constructor(){
-        this.id= 1;
-        this.nombre= 'Básica incompleta';
+  
+    public constructor() {
+      this.id = 1;
+      this.nombre = 'Básica Incompleta';
     }
-
-    public setNivelEducacional(id:number, nombre:string): void{
-        this.id= id;
-        this.nombre= nombre;
+  
+    public setNivelEducacional(id: number, nombre: string): void {
+      this.id = id;
+      this.nombre = nombre;
     }
-
-    public getNivelEducacional(id: number, nombre: string): NivelEducacional{
-        const nivel= new NivelEducacional();
-        nivel.setNivelEducacional(id,nombre)
-        return nivel;
+  
+    public static getNivelEducacional(id: number, nombre: string): NivelEducacional {
+      const nivel = new NivelEducacional();
+      nivel.setNivelEducacional(id, nombre);
+      return nivel;
     }
-
-    public static getNivelesEducacionales(): NivelEducacional[]{
-        const niveles: NivelEducacional[]=[
-            new NivelEducacional().getNivelEducacional(1, 'Básica incompleta'),
-            new NivelEducacional().getNivelEducacional(2, 'Básica completa'),
-            new NivelEducacional().getNivelEducacional(3, 'Media incompleta'),
-            new NivelEducacional().getNivelEducacional(4, 'Media completa'),
-            new NivelEducacional().getNivelEducacional(5, 'Superior incompleta'),
-            new NivelEducacional().getNivelEducacional(6, 'Superior completa'),
-        ];
-        return niveles;
+  
+    public static getNivelesEducacionales(): NivelEducacional[] {
+      const niveles: NivelEducacional[] = [
+        NivelEducacional.getNivelEducacional(1, 'Básica Incompleta'),
+        NivelEducacional.getNivelEducacional(2, 'Básica Completa'),
+        NivelEducacional.getNivelEducacional(3, 'Media Incompleta'),
+        NivelEducacional.getNivelEducacional(4, 'Media Completa'),
+        NivelEducacional.getNivelEducacional(5, 'Superior Incompleta'),
+        NivelEducacional.getNivelEducacional(6, 'Superior Completa')
+      ];
+      return niveles;
     }
-    public getTextoNivelEducacional(): string{
-        return this.id.toString()+'-'+ this.nombre;
+    
+    public getEducacion(): string {
+      return this.id.toString() + ' - ' + this.nombre;
     }
-
-    public static findNivelEducacionalById(id:number): NivelEducacional | undefined{
-        return NivelEducacional.getNivelesEducacionales().find(n => n.id === id);
+  
+    public static buscarNivelEducacional(id: number): NivelEducacional | undefined {
+      return NivelEducacional.getNivelesEducacionales().find(n => n.id === id);
     }
-}
+  
+  }
--- src/app/pages/inicio/inicio.module.ts
+++ src/app/pages/inicio/inicio.module.ts
@@ -1,20 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
-
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { InicioPageRoutingModule } from './inicio-routing.module';
-
 import { InicioPage } from './inicio.page';
+import { RouteReuseStrategy } from '@angular/router';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+
+// CGV: Para usar Angular Material se deben agregar los sguientes módulos
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    InicioPageRoutingModule
+      CommonModule
+    , FormsModule
+    , IonicModule
+    , InicioPageRoutingModule
+    , MatDatepickerModule
   ],
-  declarations: [InicioPage]
+  providers: [
+    {
+      provide: RouteReuseStrategy,
+      useClass: IonicRouteStrategy
+    },
+  ],
+  declarations: [InicioPage],
 })
 export class InicioPageModule {}
